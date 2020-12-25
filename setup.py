00195c34766549ebf994c1b9dc3ff95f72f0ed89
Aaron Elliot <aaron.elliot.dev@gmail.com>
2020-12-25 05:24:54 +0000

    * added find_packages() w

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='gym_music',
       version='0.0.1',
-      install_requires=[]
+      packages = find_packages(),
      )
 
