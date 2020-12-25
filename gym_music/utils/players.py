999a4d7c0c5bc105130ed395efa8fbea004c7fe6
Aaron Elliot <aaron.elliot.dev@gmail.com>
2020-12-25 03:45:57 +0000

    *removed egg from tracking *added pygame to requirements.txt *modified interface of player to use queue() instead of play() *implemented player as MIDIPlayer using pygame

--- gym_music/utils/players.py
+++ gym_music/utils/players.py
@@ -1,14 +1,11 @@
-
+import pygame
 
 class Player():
 
   def __init__(self):
     pass
-  
-  def run(self):
-    pass
 
-  def play(self, content_path):
+  def queue(self,content_path):
     pass
 
   def close(self):
@@ -20,6 +17,20 @@ class MIDI_Player(Player):
 
   def __init__(self):
     super(MIDI_Player, self).__init__()
+    pygame.mixer.init()
+
+  def queue(self,content_path):
+
+    if pygame.mixer.music.get_busy(): # if currently playing music
+      pygame.mixer.music.queue(content_path)
+    else:
+      pygame.mixer.music.load(content_path)
+      pygame.mixer.music.play()
+
+
+  def close(self):
+    pygame.mixer.quit()
+
 
 
 
