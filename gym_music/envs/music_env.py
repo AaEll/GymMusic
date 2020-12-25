ab611927f4ab1b8f1d2d80689ecbad00b7cd1af0
Aaron Elliot <aaron.elliot.dev@gmail.com>
2020-12-25 03:55:35 +0000

    * added stop condition by number of rounds * fixed syntax errors

--- gym_music/envs/music_env.py
+++ gym_music/envs/music_env.py
@@ -1,51 +1,64 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
-#...
-#from ..utils.builders import MidiBuilder
-#from ..utils.players import MidiPlayer
+
+from ..utils.builders import MidiBuilder
+from ..utils.players import MidiPlayer
+
+
 class MusicEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
-  def __init__(self):
+  def __init__(self, max_rounds = 10):
     super(MidiEnv,self).__init__()
     self.action_space = spaces.MultiDiscrete([2]*10)
     self.observation_space = spaces.Discrete(2)
     
     # define Midi utility objects
-    #self.builder = MidiBuilder()
-    #self.player = MidiPlayer()
+    self.builder = MidiBuilder()
+    self.player = MidiPlayer()
+
+    # stop condition number of rounds
+    self._proto_rounds = 0
+    self.max_proto_rounds = max_rounds
 
 
   def step(self, action):
 
-    if is_stop_action(action):
-        #midi_file_path = self.builder.build()
-        #song_id = self.player.queue(midi_file_path)
-        #reward = self.player.wait(song_id)
-        reward = 1
-        obs = 1
-        done = 1
+    self._proto_rounds = self._proto_rounds + 1
+
+    if self.is_stop_action(action):
+      self.builder.append(action)
+      midi_file_path = self.builder.build()
+      self.player.queue(midi_file_path)
+
+      #reward = self.player.wait(song_id)
+      reward = 1
+      obs = 1
+      done = 1
 
     else:
-        #self.builder.append(action)
-        reward = 0
-        obs = 0
-        done = 0
+      self.builder.append(action)
+      reward = 0
+      obs = 0
+      done = 0
+
     return obs,reward,done, {}
 
-
-
   def reset(self):
-    self.midi_builder.reset()
+    self.builder.reset()
+    self.player.reset()
     return None
   
   def render(self, mode='human',):
     pass
 
   def close(self):
-    #self.builder.close()
-    #self.player.close()
+    self.builder.close()
+    self.player.close()
     pass
 
+  def _is_stop_action(action):
+    return self._proto_rounds >= self.max_proto_rounds
+    
 
