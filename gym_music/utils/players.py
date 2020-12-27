b24d9bcdfdb309d278fbe62587eef05e0fc0ddf2
Aaron Elliot <aaron.elliot.dev@gmail.com>
2020-12-27 02:30:25 +0000

    * replaced pygame audio player with asyncio callingwqx fluidsynth

--- gym_music/utils/players.py
+++ gym_music/utils/players.py
@@ -1,4 +1,7 @@
-import pygame
+
+import asyncio
+import threading
+
 
 class Player():
 
@@ -15,29 +18,56 @@ class Player():
     pass
   
 
+def threaded(fn):
+  def wrapper(*args, **kwargs):
+    thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+    thread.start()
+    return thread
+  return wrapper
+
+async def run_subprocess(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    stdout, stderr = await proc.communicate()
+    
+    """
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        print(f'[stdout]\n{stdout.decode()}')
+    if stderr:
+        print(f'[stderr]\n{stderr.decode()}')
+    """
+    return proc, stdout, stderr
 
 class MidiPlayer(Player):
-
+  open_audio_cmd = "fluidsynth -i -a pulseaudio /usr/share/sounds/sf2/FluidR3_GM.sf2 {}"
   def __init__(self):
     super(MidiPlayer, self).__init__()
-    pygame.mixer.init()
-
+    self._sub_process = None # current subprocess
+    self._lock = asyncio.Semaphore(value=1)
+  
   def queue(self,content_path):
-
-    if pygame.mixer.music.get_busy(): # if currently playing music
-      pygame.mixer.music.queue(content_path)
-    else:
-      pygame.mixer.music.load(content_path)
-      pygame.mixer.music.play()
+    self._queue(content_path)
 
   def reset(self):
-    pygame.mixer.quit()
-    pygame.mixer.init()
-
+    pass
   def close(self):
-    pygame.mixer.quit()
+    pass
 
+  def is_running(self):
+    # returns None if running, 0 if not
+    poll = self._sub_process.poll()
+    return poll is None
 
+  @threaded
+  async def _queue(self,content_path):
+    await self._lock.acquire()
+    # create music process and await for it to finish
+    _ = asyncio.run_subprocess(run(self.open_audio_cmd.format(content_path)))
+    self._lock.release()
 
 
 
