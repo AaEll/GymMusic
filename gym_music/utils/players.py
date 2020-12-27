e151f5263d03f882f2c94722ecd791555803823b
Aaron Elliot <aaron.elliot.dev@gmail.com>
2020-12-27 03:19:55 +0000

    removed threading, now using only asyncio, removed parallelism for working example

--- gym_music/utils/players.py
+++ gym_music/utils/players.py
@@ -1,6 +1,5 @@
 
 import asyncio
-import threading
 
 
 class Player():
@@ -17,14 +16,6 @@ class Player():
   def reset(self):
     pass
   
-
-def threaded(fn):
-  def wrapper(*args, **kwargs):
-    thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
-    thread.start()
-    return thread
-  return wrapper
-
 async def run_subprocess(cmd):
     proc = await asyncio.create_subprocess_shell(
         cmd,
@@ -33,41 +24,41 @@ async def run_subprocess(cmd):
 
     stdout, stderr = await proc.communicate()
     
-    """
+    
     print(f'[{cmd!r} exited with {proc.returncode}]')
     if stdout:
         print(f'[stdout]\n{stdout.decode()}')
     if stderr:
         print(f'[stderr]\n{stderr.decode()}')
-    """
+    
     return proc, stdout, stderr
 
 class MidiPlayer(Player):
   open_audio_cmd = "fluidsynth -i -a pulseaudio /usr/share/sounds/sf2/FluidR3_GM.sf2 {}"
   def __init__(self):
     super(MidiPlayer, self).__init__()
-    self._sub_process = None # current subprocess
-    self._lock = asyncio.Semaphore(value=1)
-  
+    self._lock = asyncio.Semaphore(1)
+    self._loop = asyncio.get_event_loop()
+
   def queue(self,content_path):
-    self._queue(content_path)
+    print("queue-ing task")
+
+    asyncio.run(run_subprocess(self.open_audio_cmd.format(content_path)))
+    #task = self._loop.create_task(self._queue(content_path))
+
 
   def reset(self):
     pass
   def close(self):
     pass
-
-  def is_running(self):
-    # returns None if running, 0 if not
-    poll = self._sub_process.poll()
-    return poll is None
-
-  @threaded
-  async def _queue(self,content_path):
-    await self._lock.acquire()
-    # create music process and await for it to finish
-    _ = asyncio.run_subprocess(run(self.open_audio_cmd.format(content_path)))
-    self._lock.release()
+  
+  @asyncio.coroutine
+  def _queue(self,content_path):
+    print('entered queue')
+    with(self._lock):
+      print('aquired lock!')
+      # create music process and await for it to finish
+      _ = asyncio.run_until_complete(run_subprocess(self.open_audio_cmd.format(content_path)))
 
 
 
