76f60761b2f263a6082ff975512336898f7a3f4f
Aaron Elliot <aaron.elliot.dev@gmail.com>
2021-01-30 01:45:55 +0000

    added heartrate connection using pexpect

--- gym_music/utils/monitors.py
+++ gym_music/utils/monitors.py
@@ -1,3 +1,5 @@
+import pexpect
+import sys
 
 class Monitor():
   def __init__(self):
@@ -15,10 +17,15 @@ class Monitor():
 
 
 class HeartMonitor(Monitor):
-  def __init__(self):
+  def __init__(self, macAddress):
     super().__init__()
 
     self._p = None
+
+    self.macAddress = macAddress
+    self._handle = None
+    self._uuid = None
+
 
     # initialize HR monitor 
 
@@ -41,10 +48,57 @@ class HeartMonitor(Monitor):
   def _close_p(self):
     if self._p is not None:
       self._p.terminate()
+ 
+  def getHeartRate(message):
+
+    hrFormatID = message[0] & 1
+    sensorStatusID = message[0] & 6 
+
+    if hrFormatID == 0:
+        hr = message[1]
+    elif hrFormatID == 1:
+        hr = (message[2] << 8) | message[1]
+
+    return hr, sensorStatusID
+
+  def connect():
+
+    connected = False
+    self.gat_p = pexpect.spawn('gattool -b ' + self.macAddress+ ' -t random --interactive') as gat_p
+    self.gat_p.logfile = sys.stdout
+
+    while not connected:
+      self.gat_p.expect(r'\[LE\}>')
+      self.gat_p.sendline('connect')
+
+      try:
+        status = self.gat_p.expect(['Connection successful.',r'\[CON\]'],timeout = 10)
+        if not status:
+          self.gat_p.expect(r'\[LE\]>', timeout = 10)
+        connected = True
+      except pexpect.TIMEOUT:
+        print('connection failed : retrying')
+      except KeyboardInterrupt:
+        self.gat_p.close()
+        quit()
+    print('connection successful, registering') 
+    
+    self.gat_p.sendline('char-desc')
+    registered = False
+    while not registered:
+      try:
+        self.gat_p.expect( r'handle: (0x[0-9a-f]+), uuid: ([0-9a-f]{8})', timeout= 10)
+      except pexcpect.TIMEOUT:
+          print('registration failed : retrying')
+      except KeyboardInterrupt:
+        self.gat_P.close()
+        quit()
+      self._handle = self.gat_p.match.group(1).decode()
+      self._uuid = self.gat_p.match.group(2).decode()
+    print('registration successful')
 
 
-
-
-
+  def read_hr():
+    pass 
 
 
