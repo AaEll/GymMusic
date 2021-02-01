fe3835b58111aca99ac254c35ae5ff375b2d31ed
Aaron Elliot <aaron.elliot.dev@gmail.com>
2021-02-01 00:53:14 +0000

    * added output folder * changed MidiBuilder default path /output/tmp -> /output * added logic to write feedback to disk in MidiPlayer

--- gym_music/utils/players.py
+++ gym_music/utils/players.py
@@ -30,18 +30,22 @@ class MidiPlayer(Player):
 
 
   def queue(self,content_path):
-    
-    self.monitor.connect()
+    pass if self.monitor.isConnected() else self.monitor.connect()
+
     self._p = subprocess.Popen(['fluidsynth', '-i','-a','pulseaudio', self.soundfont, content_path,'-r',self.samplerate])
     total = 0
     for i in range(self.steps):
-      total = total + self.monitor.read()
+      obs = self.monitor.read()
+      total = total + obs
       time.sleep(1)
+    feedback = total/self.steps
 
     if self.isRunning():
       self._p.wait()
-
-    return total/(self.steps)
+    
+    with open(content_path+'.feedback', 'w') as f:
+      f.write('hr\n{}'.format(feedback))
+    return feedback
 
   def reset(self):
     self._close_p()
@@ -58,9 +62,6 @@ class MidiPlayer(Player):
 
 
   def isRunning(self):
-    if self._p is None:
-      return False
-    else:
-      return self._p.poll() is None
+    return (False if self._p is None else (self_p.poll is None))
 
 
